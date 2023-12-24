565bb1ea486e5ac089cff666c6c9939fe5e67f7e
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2023-12-24 05:56:05 -0800

    Initial release

---
+++ ytdlpUpdater/ytdlpUpdater.cs
@@ -0,0 +1,20 @@
+﻿using ResoniteModLoader;
+using System.Diagnostics;
+
+namespace ytdlpUpdater;
+
+public class ytdlpUpdater : ResoniteMod
+{
+    public override string Name => "yt-dlp-Updater";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/Raidriar796/yt-dlp-Updater";
+
+    public override void OnEngineInit()
+    {
+        Process process = new Process();
+        process.StartInfo.FileName = "RuntimeData/yt-dlp.exe";
+        process.StartInfo.Arguments = "-U";
+        process.Start();
+    }
+}
